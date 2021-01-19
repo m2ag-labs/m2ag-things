317052c8d21323564c7ad3a774e9c9d956da6872
marc <mbgraham2000@gmail.com>
2021-01-18 17:03:23 -0800

    don't for get to convert address to int

--- components/veml7700/veml7700.py
+++ components/veml7700/veml7700.py
@@ -1,4 +1,7 @@
-import adafruit_veml7700
+try:
+    import adafruit_veml7700
+except ModuleNotFoundError:
+    print('veml7700 driver not installed')
 
 # https://www.adafruit.com/product/4162?utm_source=youtube&amp;utm_medium=videodescrip&amp;utm_campaign=newproducts&gclid=CjwKCAjwyo36BRAXEiwA24CwGVaaUUkdGNnE0tz5silh9XHsYxFG_sCalH0KaaAqhwkw4Bbo4OOIVRoCJlYQAvD_BwE
 # https://github.com/adafruit/Adafruit_CircuitPython_VEML7700
@@ -9,7 +12,7 @@ import adafruit_veml7700
 class Veml7700:
 
     def __init__(self, i2c, config, logging):
-        self.veml7700 = adafruit_veml7700.VEML7700(i2c, address=config['init']['address'])
+        self.veml7700 = adafruit_veml7700.VEML7700(i2c, address=int(config['init']['address'],16))
 
     def get(self, val):
         if val == 'light':
