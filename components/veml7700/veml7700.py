13f47e3df9c4acaecce3e912abfb59b8abac26d0
marc <mbgraham2000@gmail.com>
2021-01-18 16:33:38 -0800

    update module

--- components/veml7700/veml7700.py
+++ components/veml7700/veml7700.py
@@ -4,12 +4,12 @@ import adafruit_veml7700
 # https://github.com/adafruit/Adafruit_CircuitPython_VEML7700
 # sudo pip3 install adafruit-circuitpython-veml7700
 # https://circuitpython.readthedocs.io/projects/veml7700/en/latest/api.html
-
+# address = 0x18
 
 class Veml7700:
 
-    def __init__(self, i2c, address=0x18):
-        self.veml7700 = self.veml7700 = adafruit_veml7700.VEML7700(i2c, address=address)
+    def __init__(self, i2c, config, logging):
+        self.veml7700 = adafruit_veml7700.VEML7700(i2c, address=config['init']['address'])
 
     def get(self, val):
         if val == 'light':
