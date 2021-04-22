024307558de76e3bb6f435cd50efd3b0015e2dfd
marc <mbgraham2000@gmail.com>
2021-04-22 14:23:48 -0700

    prune component/things. Hardware only.

--- extras/raspialarm/components/backlight/backlight.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo sh -c 'echo "0" > /sys/class/backlight/soc\:backlight/brightness'
-gpio -g mode "$1" pwm
-gpio pwmc 1000
-gpio -g pwm "$1" 512 #set display to half bright as default
